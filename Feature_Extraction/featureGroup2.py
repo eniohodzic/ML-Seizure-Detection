d018e257c56d69023ed29440ef497cc41951ac29
eniohodzic <ehodzic@g.ucla.edu>
2022-11-03 22:28:11 -0700

    features extraction pipeline

---
+++ Feature_Extraction/featureGroup2.py
@@ -0,0 +1,36 @@
+"""
+More complicated features that perform transformations on signal
+"""
+
+import pandas as pd 
+
+def feature2A(signal: pd.DataFrame):
+    """
+    FeatureA (Example): Fourier transform into frequency domain 
+
+    Input: Post-processed signal in Pandas DataFrame format
+        Other constants dependent on the feature
+
+    Output: Feature in either numpy array or Pandas DataFrame
+    """
+
+    return []
+
+def feature2B(signal: pd.DataFrame):
+    """
+    FeatureB (Example): Discrete wavelet transform 
+
+    Input: Post-processed signal in Pandas DataFrame format
+        Other constants dependent on the feature
+
+    Output: Feature in either numpy array or Pandas DataFrame
+    """
+
+    return []
+
+def feature2X(signal: pd.DataFrame):
+    """
+    Continue adding features that correlate with pre-seizure indication
+    """
+
+    return []
