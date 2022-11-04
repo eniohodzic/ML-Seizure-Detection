626c21dddaeec181ac21324a8f88012202334814
eniohodzic <ehodzic@g.ucla.edu>
2022-11-04 11:48:54 -0700

    Reorganize

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+This file will run all of the components
+
+1) Data import
+2) Data pre-processing and filtering 
+3) Data formatting 
+4) Feature extraction
+5) Feature selection 
+6) Model training and testing
+"""
