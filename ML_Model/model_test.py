01750821d51b7c8c862ecacddf7d57ac023e4bfe
kevinmjung <43766468+kevinmjung@users.noreply.github.com>
2022-11-04 20:17:28 -0700

    update readme and ml test

--- ML_Model/model_test.py
+++ ML_Model/model_test.py
@@ -1,4 +1,5 @@
 from modelBuild import *
 samp_feat, samp_sig = None, None
 x,y = formatData(samp_feat, samp_sig)
-a,b,c,d = splitData(x,y)
+X_train, X_test, y_train, y_test = splitData(x,y)
+#tests for all variables above
